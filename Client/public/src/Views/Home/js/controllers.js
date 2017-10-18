15fe9737cc3773e8fa6964adb21580952529edc3
Eric Catlin <eroccatlun@gmail.com>
2017-10-17 22:19:54 -0700

    Bringing ClickerGame client app into the fold

--- Client/public/src/Views/Home/js/controllers.js
+++
@@ -1,3 +0,0 @@
-function homeViewCtrl($scope, HomeAPI, GraphJS) {
-   
-}
---
+++ Client/src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: true,
+
+    region: 'us-west-2',
+
+    identityPoolId: 'us-west-2:f7af784b-bc20-418c-8a09-9952836b100f',
+    userPoolId: 'us-west-2_8sEsk8XKn',
+    clientId: 'hngoi3enb10s3s5phql428sju',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+
+};
