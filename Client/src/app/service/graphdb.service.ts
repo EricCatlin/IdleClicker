23247f5359f6d7787f312b946df5f7fb3ec694cc
Catlin, Eric (HA Group) <ECatlin@hollandamericagroup.com>
2017-10-24 15:07:58 -0700

    graphdb service

--- Client/src/app/service/graphdb.service.ts
+++ Client/src/app/service/graphdb.service.ts
@@ -1,13 +1,9 @@
 import {Injectable} from "@angular/core";
 import {CognitoUtil} from "./cognito.service";
 import {environment} from "../../environments/environment";
-
 import {Stuff} from "../secure/useractivity/useractivity.component";
 import * as AWS from "aws-sdk";
-
-
-AWS.config.region = 'us-west-2';  //us-west-2 is Oregon
-
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Created by Eric Catlin
@@ -15,15 +11,28 @@ AWS.config.region = 'us-west-2';  //us-west-2 is Oregon
 
 @Injectable()
 export class GraphDBService {
-    constructor(public cognitoUtil: CognitoUtil) {
+    constructor(public cognitoUtil: CognitoUtil, private http: HttpClient) {
         console.log("GraphDBService: constructor");
     }
 
     getAWS() {
         return AWS;
     }
+    results: string[];
     getSavedGames() {
-       
+        console.log("tried to get saved games");
+        this.http.get('http://localhost:8080/api/games/savedgames/f43f6320-f198-4dea-8809-0e03dfba6fb2').subscribe(data => {
+            // Read the result field from the JSON response.
+            return data;
+          });
+      
+    }
+    writeLogEntry(action: string){
+        console.log("tried to write log entries",action);        
+    }
+
+    getLogEntries(logs: Stuff[]){
+        console.log("tried to get log entries",logs);
     }
    
 
